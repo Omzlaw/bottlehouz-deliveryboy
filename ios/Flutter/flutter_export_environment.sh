9ab97c7fdf2d3970267e14432566911da4c42f93
Omzlaw <omeizaalabi@gmail.com>
2022-06-03 12:22:34 +0100

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\LENOVO\Downloads\Compressed\Grocery Food\deliveryboy-130\codecanyon-26615174-delivery-boy-for-groceries-foods-pharmacies-stores-flutter-app\flutter_application"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.4.0"
+export "FLUTTER_BUILD_NUMBER=5"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
